ad2539d99907c978754b819eb3bedc645434657d
mstombaugh <mstombaugh001@gmail.com>
2014-03-26 18:58:47 -0500

    Update DB_Functions.php

--- DB_Functions.php
+++ DB_Functions.php
@@ -116,7 +116,7 @@ class DB_Functions {
  
         $salt = sha1(rand());
         $salt = substr($salt, 0, 10);
-        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $encrypted = base64_encode(sha1($password . $salt) . $salt);
         $hash = array("salt" => $salt, "encrypted" => $encrypted);
         return $hash;
     }
@@ -128,7 +128,7 @@ class DB_Functions {
      */
     public function checkhashSSHA($salt, $password) {
  
-        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = base64_encode(sha1($password . $salt) . $salt);
  
         return $hash;
     }
