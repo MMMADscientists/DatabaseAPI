6f372a1b324102463cfbb8d3258f6a245fce89db
mstombaugh <mstombaugh001@gmail.com>
2014-03-26 17:39:28 -0500

    Update DB_Connect.php

--- DB_Connect.php
+++ DB_Connect.php
@@ -16,7 +16,7 @@ class DB_Connect {
     public function connect() {
         require_once 'config.php';
         // connecting to mysql
-        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+        $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
         // selecting database
         mysql_select_db(DB_DATABASE);
  
