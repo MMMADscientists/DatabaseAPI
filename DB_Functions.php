6da4e415f802fa1e7fc198a06af2d37019c47077
mstombaugh <mstombaugh001@gmail.com>
2014-03-26 19:05:14 -0500

    Update DB_Functions.php

--- DB_Functions.php
+++ DB_Functions.php
@@ -48,8 +48,8 @@ class DB_Functions {
             $uid = $this->mysql->insert_id; // last inserted id
             $result = $this->mysql->query("SELECT * FROM User WHERE uid = $uid");
             // return user details
-            echo PHP_EOL
-            echo '$result->fetch_array(MYSQLI_ASSOC)';
+            echo PHP_EOL;
+            echo  $result->fetch_array(MYSQLI_ASSOC);
             return $result->fetch_array(MYSQLI_ASSOC);
         } else {
             return false;
